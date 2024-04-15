6579eda14e127c524e19faadd0f4492e559c5fd2
KimHagyung <joajoa171717@gmail.com>
2024-04-15 19:24:39 +0900

    마커 및 앱실행시 현위치 즉시설정

--- app/src/main/java/com/example/moduroad/NaverMapFragment.kt
+++ app/src/main/java/com/example/moduroad/NaverMapFragment.kt
@@ -1,195 +1,56 @@
+import android.content.Context
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import com.naver.maps.geometry.LatLng
 import com.naver.maps.map.MapFragment
-import com.naver.maps.map.OnMapReadyCallback
 import com.naver.maps.map.NaverMap
-import com.naver.maps.map.overlay.Marker
+import com.naver.maps.map.OnMapReadyCallback
 import com.naver.maps.map.util.FusedLocationSource
-import com.google.android.gms.location.LocationCallback
-import com.google.android.gms.location.LocationRequest
-import com.google.android.gms.location.LocationResult
 import com.google.android.gms.location.FusedLocationProviderClient
 import com.google.android.gms.location.LocationServices
-import android.widget.Toast
-import androidx.core.app.ActivityCompat
-import android.Manifest
-import android.content.pm.PackageManager
+import com.naver.maps.map.LocationTrackingMode
+import com.naver.maps.geometry.LatLng
 import com.naver.maps.geometry.LatLngBounds
 import com.naver.maps.map.CameraUpdate
 
 class MyMapFragment : MapFragment(), OnMapReadyCallback {
     private lateinit var locationSource: FusedLocationSource
     private lateinit var fusedLocationClient: FusedLocationProviderClient
-    private var locationRequest: LocationRequest? = null
-    private var locationCallback: LocationCallback? = null
-    private var currentMarker: Marker? = null
-    private lateinit var naverMap: NaverMap
+    private lateinit var naverMap: NaverMap // 여기에 naverMap 변수를 선언합니다.
     private val LOCATION_PERMISSION_REQUEST_CODE = 1000
 
-    override fun onCreateView(
-        inflater: LayoutInflater,
-        container: ViewGroup?,
-        savedInstanceState: Bundle?
-    ): View? {
+    override fun onAttach(context: Context) {
+        super.onAttach(context)
+        fusedLocationClient = LocationServices.getFusedLocationProviderClient(context)
+    }
+
+    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
         val rootView = super.onCreateView(inflater, container, savedInstanceState)
-
-        // Initialize FusedLocationProviderClient
-        fusedLocationClient = LocationServices.getFusedLocationProviderClient(activity!!)
-
-        // Asynchronously receive the map object
         getMapAsync(this)
-
-        // Initialize the location source
         locationSource = FusedLocationSource(this, LOCATION_PERMISSION_REQUEST_CODE)
-
         return rootView
     }
 
     override fun onMapReady(naverMap: NaverMap) {
-        this.naverMap = naverMap
-        naverMap.locationOverlay.isVisible = false
+        this.naverMap = naverMap // 여기에서 naverMap 변수를 초기화합니다.
         naverMap.uiSettings.isLocationButtonEnabled = true
         naverMap.locationSource = locationSource
 
-        setupMap()
-        setupLocationRequest()
-        setupLocationCallback()
-        updateCurrentLocationAndMoveCamera()  // Move camera to current location at start
-        startLocationUpdates()
+        // 현위치 추적 모드 활성화
+        naverMap.locationTrackingMode = LocationTrackingMode.Follow
+
+
     }
 
-    private fun setupMap() {
-        naverMap.minZoom = 10.0
-        naverMap.maxZoom = 18.0
-        val inchonBounds = LatLngBounds(LatLng(37.2830, 126.3920), LatLng(37.5580, 126.7780))
-        naverMap.extent = inchonBounds
-        val initialPosition = LatLng(37.4563, 126.7052)
-        naverMap.moveCamera(CameraUpdate.scrollTo(initialPosition))
-    }
-
-    private fun setupLocationRequest() {
-        locationRequest = LocationRequest.create().apply {
-            interval = 5000  // Update location every 5 seconds
-            fastestInterval = 2500
-            priority = LocationRequest.PRIORITY_HIGH_ACCURACY
-        }
-    }
-
-    private fun setupLocationCallback() {
-        locationCallback = object : LocationCallback() {
-            override fun onLocationResult(locationResult: LocationResult) {
-                locationResult.locations.lastOrNull()?.let { location ->
-                    val newLatLng = LatLng(location.latitude, location.longitude)
-                    if (currentMarker == null) {
-                        currentMarker = Marker().apply {
-                            position = newLatLng
-                            map = naverMap
-                        }
-                    } else {
-                        currentMarker!!.position = newLatLng
-                    }
-                    // No camera movement here
-                }
+    // 위치 권한 요청 결과 처리
+    override fun onRequestPermissionsResult(requestCode: Int, permissions: Array<out String>, grantResults: IntArray) {
+        if (locationSource.onRequestPermissionsResult(requestCode, permissions, grantResults)) {
+            if (!locationSource.isActivated) { // 권한 거부됨
+                naverMap.locationTrackingMode = LocationTrackingMode.None
             }
-        }
-    }
-
-    private fun updateCurrentLocationAndMoveCamera() {
-        if (ActivityCompat.checkSelfPermission(
-                requireContext(),
-                Manifest.permission.ACCESS_FINE_LOCATION
-            ) != PackageManager.PERMISSION_GRANTED &&
-            ActivityCompat.checkSelfPermission(
-                requireContext(),
-                Manifest.permission.ACCESS_COARSE_LOCATION
-            ) != PackageManager.PERMISSION_GRANTED
-        ) {
-            ActivityCompat.requestPermissions(
-                requireActivity(),
-                arrayOf(
-                    Manifest.permission.ACCESS_FINE_LOCATION,
-                    Manifest.permission.ACCESS_COARSE_LOCATION
-                ),
-                LOCATION_PERMISSION_REQUEST_CODE
-            )
             return
         }
-        fusedLocationClient.lastLocation.addOnSuccessListener { location ->
-            location?.let {
-                val currentLatLng = LatLng(location.latitude, location.longitude)
-                naverMap.moveCamera(CameraUpdate.scrollTo(currentLatLng))
-                if (currentMarker == null) {
-                    currentMarker = Marker().apply {
-                        position = currentLatLng
-                        map = naverMap
-                    }
-                } else {
-                    currentMarker!!.position = currentLatLng
-                }
-            }
-        }
-    }
-
-    private fun startLocationUpdates() {
-        if (ActivityCompat.checkSelfPermission(
-                requireContext(),
-                Manifest.permission.ACCESS_FINE_LOCATION
-            ) != PackageManager.PERMISSION_GRANTED &&
-            ActivityCompat.checkSelfPermission(
-                requireContext(),
-                Manifest.permission.ACCESS_COARSE_LOCATION
-            ) != PackageManager.PERMISSION_GRANTED
-        ) {
-            ActivityCompat.requestPermissions(
-                requireActivity(),
-                arrayOf(
-                    Manifest.permission.ACCESS_FINE_LOCATION,
-                    Manifest.permission.ACCESS_COARSE_LOCATION
-                ),
-                LOCATION_PERMISSION_REQUEST_CODE
-            )
-            return
-        }
-        locationRequest?.let { req ->
-            locationCallback?.let { callback ->
-                fusedLocationClient.requestLocationUpdates(req, callback, null)
-            } ?: run {
-                // locationCallback이 null일 경우의 처리 로직
-                Toast.makeText(
-                    requireContext(),
-                    "Location callback is not initialized",
-                    Toast.LENGTH_SHORT
-                ).show()
-            }
-        }
-    }
-
-
-    override fun onRequestPermissionsResult(
-        requestCode: Int,
-        permissions: Array<out String>,
-        grantResults: IntArray
-    ) {
         super.onRequestPermissionsResult(requestCode, permissions, grantResults)
-        if (requestCode == 1000 && grantResults.isNotEmpty() && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
-            startLocationUpdates()
-        } else {
-            Toast.makeText(requireContext(), "Location permission is required.", Toast.LENGTH_LONG)
-                .show()
-        }
-    }
-
-    override fun onPause() {
-        super.onPause()
-        stopLocationUpdates()
-    }
-
-    private fun stopLocationUpdates() {
-        locationCallback?.let { callback ->
-            fusedLocationClient.removeLocationUpdates(callback)
-        }
     }
 }
