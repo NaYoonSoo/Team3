97f8703ad463d9601806fd380e917030304ff263
suys65 <suysinu65@gmail.com>
2024-05-15 23:58:57 +0900

    서버 추가

---
+++ moduroad/server/moduroad_app/add_obstacle.py
@@ -0,0 +1,113 @@
+from inference_sdk import InferenceHTTPClient
+from PIL import Image
+from flask import Flask, request, jsonify
+import osmnx as ox
+
+CLIENT = InferenceHTTPClient(
+    api_url="https://detect.roboflow.com",
+    api_key="TIcrpEjP7QDYM8tnrZG4"
+)
+
+
+def calculate_edge_weights(edge_data, e_length_weight, e_obstacle_weights, w_length_weight, w_obstacle_weights):
+  
+    # e_weight 계산
+    e_total_weight = edge_data.get('length', 0) * e_length_weight
+    for obstacle, weight in e_obstacle_weights.items():
+        if obstacle in edge_data and edge_data[obstacle] != 0:
+            num_obstacles = len(edge_data[obstacle]) if isinstance(edge_data[obstacle], list) else 0
+            e_total_weight += num_obstacles * weight
+    edge_data['e_weight'] = e_total_weight
+
+    # w_weight 계산
+    w_total_weight = edge_data.get('length', 0) * w_length_weight
+    for obstacle, weight in w_obstacle_weights.items():
+        if obstacle in edge_data and edge_data[obstacle] != 0:
+            num_obstacles = len(edge_data[obstacle]) if isinstance(edge_data[obstacle], list) else 0
+            w_total_weight += num_obstacles * weight
+    edge_data['w_weight'] = w_total_weight
+
+    return edge_data
+#---------------------------------------------------------------------------------------------------------------------------#
+# e_weight 계산을 위한 가중치 (노약자)
+e_length_weight = 0.164
+e_obstacle_weights = {
+    'slope': 0.195,
+    'stair_steep': 0.268,
+    'bollard': 0.069,
+    'crosswalk_curb': 0.069,
+    'sidewalk_curb': 0.069
+}
+
+# w_weight 계산을 위한 가중치 (휠체어)
+w_length_weight = 0.045
+w_obstacle_weights = {
+    'slope': 0.182,
+    'stair_steep': 10,
+    'bollard': 0.253,
+    'crosswalk_curb': 0.253,
+    'sidewalk_curb': 0.253
+}
+
+
+#------------------------------------------------------------------------------------------------------------------------------------------#
+# 장애물 가까이에 있는 엣지 가중치 증가 함수 정의
+def update_edge_weights(G, point, attribute_name):
+    # 가장 가까운 엣지 찾기
+    G.graph['crs'] = 'epsg:4326'
+    nearest_edge_nodes = ox.distance.nearest_edges(G, X=point.x, Y=point.y)  # OSMnx는 (y, x) 형식으로 좌표를 사용합니다.
+    # nearest_edge_nodes는 (u, v, key) 형태의 튜플입니다.
+    u, v, key = nearest_edge_nodes
+    # 해당 엣지의 데이터 가져오기
+    nearest_edge = G[u][v][key]   #edge_data
+    print(nearest_edge)
+    # 가장 가까운 엣지의 가중치 업데이트
+    # 시작 지점에 마커 추가
+    
+    if attribute_name in nearest_edge:
+    # 속성 값이 0인지 확인합니다.
+        if nearest_edge[attribute_name] == 0:
+            # 속성 값이 0이라면, 새로운 포인트를 포함하는 리스트로 변경합니다.
+            nearest_edge[attribute_name] = [point]
+        elif isinstance(nearest_edge[attribute_name], list):
+            # 속성 값이 리스트인 경우, 새로운 포인트를 리스트에 추가합니다.
+            nearest_edge[attribute_name].append(point)
+        else:
+            # 속성 값이 0이 아니고 리스트도 아닌 다른 값인 경우, 그 값을 리스트에 넣고 새로운 포인트도 추가합니다.
+            current_value = nearest_edge[attribute_name]
+            nearest_edge[attribute_name] = [current_value, point]
+            #print("change")
+        print(nearest_edge)
+        # 함수 호출
+        nearest_edge = calculate_edge_weights(nearest_edge, e_length_weight, e_obstacle_weights, w_length_weight, w_obstacle_weights)
+        print(nearest_edge)
+        return G, jsonify({"success": True,
+                           "obstacles": attribute_name})
+#-----------------------------------------------------------------------------------------------------------------------------------#
+    
+
+def add_obstacles(image, local, G):
+    G.graph['crs'] = 'epsg:4326'
+    #result = CLIENT.infer(image, model_id="stairs_detection-jaj7e/2") #result1 = CLIENT.infer(image, model_id="wheelchair-g2qh2/1")
+    result = CLIENT.infer(image, model_id="up_down_stairs_v2/1")
+    result1 = CLIENT.infer(image, model_id="-1-pkbth/2")
+    result2 = CLIENT.infer(image, model_id="curbs-bxcqk/1")
+
+    # result["predictions"] 존재 여부에 따라 True 또는 False 반환
+    if "predictions" in result and result["predictions"]: #계단 감지 경우
+        return update_edge_weights(G, local, attribute_name = 'stair_steep')
+    
+    #계단이 감지되지 않음
+    elif "predictions" in result1 :
+        for prediction in result1["predictions"][:2]:
+            if prediction["class"] == "bollard" and prediction["confidence"] >=0.6:
+                return update_edge_weights(G, local, attribute_name='bollard')
+            
+    elif "predictions" in result2 :
+        for prediction in result2["predictions"][:2]:
+            if prediction["class"] == "curb" and prediction["confidence"] >=0.6:
+                return update_edge_weights(G, local, attribute_name='sidewalk_curb')
+    else :
+        return G, jsonify({"success": False})
+    
+
---
+++ moduroad/server/moduroad_app/application.py
@@ -0,0 +1,76 @@
+
+# 실행할 메인 파일
+from flask import Flask, request, jsonify
+import math
+from algorithm import find_shortest_path
+from add_obstacle import add_obstacles
+import pandas as pd
+import pickle
+from inference_sdk import InferenceHTTPClient
+import networkx as nx
+from PIL import Image
+import io
+import osmnx as ox
+from shapely.geometry import Point
+
+
+app = Flask(__name__, static_url_path='')
+
+CLIENT = InferenceHTTPClient(
+    api_url="https://detect.roboflow.com",
+    api_key="TIcrpEjP7QDYM8tnrZG4"
+)
+
+
+global network_g
+
+file_path = 'C:/my_code/moduroad/moduroad_app/cashe_data/Wheelchair_graph.pickle'
+
+with open(file_path, 'rb') as file:
+    network_g = pickle.load(file)
+    
+    
+
+
+@app.route('/find-path', methods=['POST'])
+def find_path_api():
+    data = request.json
+    start = (data['start_lat'], data['start_lon'])
+    end = (data['end_lat'], data['end_lon'])
+
+    type = data['type']
+    route ,distance, time, obstacle = find_shortest_path(start, end, network_g, type)
+
+    # 여기서는 경로(route)를 직접 반환하고 있으나, 실제로는 경로에 대한 상세 정보를 제공하는 것이 좋습니다.
+    return jsonify({'route': route, 'distance' : distance,'time':time, 'obstacle':obstacle})
+
+
+
+@app.route('/detect', methods=['POST'])
+def detect():
+    global network_g
+    if 'image' not in request.files:
+        return '이미지 파일이 없습니다.', 400
+
+    image_file = request.files['image']
+    longitude = request.form['longitude']
+    latitude = request.form['latitude']
+    longitude = float(longitude)
+    latitude = float(latitude)
+
+    local= Point(longitude, latitude)
+
+    image_bytes = image_file.read()
+    image = Image.open(io.BytesIO(image_bytes))
+    if image.mode == 'RGBA':
+        image = image.convert('RGB')
+        
+    network_g, result = add_obstacles(image, local, G = network_g)
+
+    return result
+    
+
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
