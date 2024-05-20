1d3888343ce8bc5e76dc786b2db5d9fcd01f400c
suys65 <suysinu65@gmail.com>
2024-05-21 02:23:05 +0900

    쏘리 이거 커밋 안함

--- app/src/main/java/com/example/moduroad/placeAPI/NaverAPI.kt
+++ app/src/main/java/com/example/moduroad/placeAPI/NaverAPI.kt
@@ -1,5 +1,3 @@
-/* 네이버 검색 API를 호출하기 위한 Interface 정의 */
-
 package com.example.moduroad.placeAPI
 
 import retrofit2.Call
@@ -9,12 +7,13 @@ import retrofit2.http.Query
 
 interface NaverAPI {
     @GET("search/local.json")
-    fun searchPlaces(
+    fun searchPlacesByLocation(
         @Header("X-Naver-Client-Id") clientId: String,
         @Header("X-Naver-Client-Secret") clientSecret: String,
         @Query("query") query: String,
         @Query("display") display: Int,
         @Query("start") start: Int,
-        @Query("sort") sort: String
+        @Query("sort") sort: String,
+
     ): Call<SearchResponse>
 }
