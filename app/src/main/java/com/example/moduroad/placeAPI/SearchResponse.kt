6ae97af37bd626f3f863953aa57537470ef42e92
suys65 <suysinu65@gmail.com>
2024-05-21 01:43:34 +0900

    목적지 검색

--- app/src/main/java/com/example/moduroad/placeAPI/SearchResponse.kt
+++ app/src/main/java/com/example/moduroad/placeAPI/SearchResponse.kt
@@ -3,16 +3,26 @@
 package com.example.moduroad.placeAPI
 
 data class SearchResponse(
-    val items: List<Place>
+    val items: List<Item>
 )
-
-data class Place(
+data class Item(
     val title: String,
-    val address: String,
     val roadAddress: String,
+    val address: String,
     val display: Int,
     val start: Int,
     val sort: String,
-    val lon: Double,
+    val mapx: Double,
+    val mapy: Double // This should match the actual response property name for latitude
+       // This should match the actual response property name for longitude
+)
+data class Place(
+    val title: String,
+    val roadAddress: String,
+    val address: String,
+    val display: Int,
+    val start: Int,
+    val sort: String?,
+    val lng: Double,
     val lat: Double
 )
